a1b266448dc144d35fcd3fe6e7f26ae8a4dc743a
Collin Blomquist <blomquic@blomquic-8shtd7.wifi.gene.com>
2018-08-15 11:26:28 -0700

    Refactored, ES6, babel, Jasmine, etc.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import "babel-polyfill";
+
+const express = require('express');
+const app = express();
+
+import Verify from './modules/Verify'
+import Utilities from './modules/Utilities'
+
+const port = process.env.PORT || 443;
+
+app.use((req, res, next) => {
+    let body = '';
+
+    req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+
+    req.on('end', () => {
+        req.body = JSON.parse(body)
+        req.rawBody = body;
+        next()
+    });
+})
+
+app.post('/', async (req, res) => {
+    try {
+        Verify.certificateURL(req.headers.signaturecertchainurl)
+        await Verify.signature(req.headers.signaturecertchainurl, req.headers.signature, req.rawBody)
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+const server = app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
+
+
---
+++ src/modules/Verify.js
@@ -0,0 +1,118 @@
+import "babel-polyfill";
+
+const axios = require('axios')
+import nodeRSA from 'node-rsa'
+const x509 = require('x509.js')
+const getPem = require('rsa-pem-from-mod-exp')
+const CryptoJS = require('crypto-js')
+const path = require('path')
+
+const timestamp = (timestamp) => {
+    const time = new Date(timestamp);
+    const currTime = new Date();
+
+    const diff = (currTime.getTime() - time.getTime()) / 1000;
+
+    if (diff <= 150 && diff >= -150) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const signature = async (url, signature, body) => {
+
+    const result = await axios.get(url);
+
+    // Grabs cert data
+    let certs = result.data;
+    certs = certs.split('-----END CERTIFICATE-----\n');
+
+    var arrSize = certs.length
+    for (var i = 0; i < arrSize - 1; i++) {
+        certs[i] = certs[i] + '-----END CERTIFICATE-----\n';
+
+        const parsedCert = x509.parseCert(certs[i])
+
+        const beforeDate = parsedCert.notBefore,
+            afterDate = parsedCert.notAfter,
+            currentDate = new Date();
+
+        if (currentDate < beforeDate || currentDate > afterDate) throw new Error('error: certificate has invalid dates')
+    }
+
+    const SAN = x509.parseCert(certs[0]).subject.commonName
+
+    if (SAN !== 'echo-api.amazon.com') throw new Error('error: certificate has invalid domain')
+
+    // check for root CA cert
+    const rootCert = x509.parseCert(certs[arrSize - 2]).issuer.organizationalUnitName;
+
+    // "Class 3 Public Primary Certification Authority"
+    if (rootCert.substring(rootCert.length - 23, rootCert.length) !== 'Certification Authority') throw new Error('error: trusted root CA certificate not found')
+
+
+
+    // extract public key
+    const cert = x509.parseCert(certs[0])
+    console.log("Begin Cert:\n\n"+JSON.stringify(cert)+"\n\nEnd Cert")
+    let modulus = x509.parseCert(certs[0]).publicModulus
+    let exp = x509.parseCert(certs[0]).publicExponent
+
+    const modulus2 = new Buffer(modulus, 'hex');
+    const exp2 = new Buffer(exp);
+    exp2.writeInt32BE(65537, 0);
+
+    const key2 = new nodeRSA()
+    key2.generateKeyPair()
+
+    modulus = new Buffer.from(modulus, 'hex').toString('base64')
+    exp = new Buffer.from(exp, 'hex').toString('base64')
+
+    const publicKey = getPem(modulus, exp)
+
+    const derivedHash = CryptoJS.SHA1(body)
+    const key = new nodeRSA(publicKey, 'pkcs1-public-pem', {signingScheme: 'pkcs1-sha1'})
+    const dec = key2.decryptPublic(signature, 'base64')
+    console.log("Decrypted: "+dec)
+
+    //console.log("assertedHash: " + assertedHash)
+    //console.log("derivedHash: " + derivedHash)
+
+    if (assertedHash !== derivedHash) {
+        throw new Error('error: hashes do not match')
+    }
+}
+
+const certificateURL = (url) => {
+    const protocol = url.substring(0, 8).toLowerCase();
+    if (protocol !== 'https://') throw new Error('error: invalid protocol')
+
+    let body = url.substring(8)
+    body = path.normalize(body)
+    body = body.replace(new RegExp(/\\/g), '/')
+
+    let array = body.split('/')
+    if (array.length !== 3) throw new Error('error: url does not have 3 elements')
+
+    switch (array[0].length) {
+        case 20:
+            if (array[0].toLowerCase().substring(0, 16) !== 's3.amazonaws.com') throw new Error('error: invalid hostname')
+            if (array[0].substring(16) !== ':443') throw new Error('error: invalid port')
+            break
+        case 16:
+            if (array[0].toLowerCase() !== 's3.amazonaws.com') throw new Error('error: invalid hostname')
+            break
+        default:
+            throw new Error('error: invalid hostname')
+    }
+
+    if (array[1] !== 'echo.api') throw new Error('error: invalid path')
+}
+
+const Verify = {
+    timestamp: timestamp,
+    signature: signature,
+    certificateURL: certificateURL
+}
+export default Verify;
