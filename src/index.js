a29e450896422f57caa0c1e51fb8bcc20e8a1513
Collin Blomquist <blomquic@blomquic-8shtd7.wifi.gene.com>
2018-08-27 11:41:07 -0700

    Refactoring of connection pool and responses

--- src/index.js
+++ src/index.js
@@ -38,13 +38,14 @@ app.post('/', async (req, res) => {
         await Verify.signature(req.headers.signaturecertchainurl, req.headers.signature, req.rawBody)
     } catch (e) {
         console.log(e)
+        return false
     }
 
     var statusCode, message, shouldEnd, titleText, contentText, instructions
     statusCode = 200;
     shouldEnd = false;
-    instructions = "Welcome to <skill name>. You can fetch information with the following commands: get records, get records in AREA, get critical records. " 
-                    + "For more information, say help."
+    instructions = "Welcome to <skill name>. You can fetch information with the following commands: get records, get records in AREA, get critical records. " +
+        "For more information, say help."
 
     // set response parameters
     res.setHeader('Content-Type', 'application/json');
@@ -53,58 +54,66 @@ app.post('/', async (req, res) => {
         titleText = "Skill Launch"
         contentText = "Launch the skill for use by saying 'Alexa, launch <skill name>.'"
     } else if (req.body.request.type === 'SessionEndedRequest') {
-        // Don't send any response
-    } else {
-        const pool = new sql.ConnectionPool(config, err => {
-            if (err) { 
-                console.log(err)
-                return;
-            }
-            pool.request().query('select TOP 2 * from ##TempTable', function(error, recordset) {
-                if (error) console.log(error)
-                else {
-                    // recordset.recordset to get relevant data
-                    console.log(recordset.recordset)
-                    //res.send(recordset)
-                    switch (req.body.request.intent.name) {
-                        case 'GetRecordIntent':
-                            message = "Test get record"
-                            titleText = "TEST"
-                            contentText = message
-                            break;
-                        case 'AMAZON.HelpIntent': 
-                            message = "To get general records, say 'Get records'. To get records from a specific area, say 'Get records in AREA', substituting in the specific area. "
-                                        + "To get records with critical status, say 'Get critical records'. To exit, say 'Stop'. To cancel operation without exiting, say 'Cancel'."
-                            titleText = "Skill Help Information"
-                            contentText = message
-                            break;
-                        case 'AMAZON.StopIntent':
-                            shouldEnd = true;
-                            message = "Skill stopped. Shutting down."
-                            titleText = "Skill Operation Stopped"
-                            contentText = "Skill operation stopped. Please relaunch to continue."
-                            break;
-                        case 'AMAZON.CancelIntent':
-                            message = "Skill operation cancelled. Please say another command to continue or 'stop' to exit."
-                            titleText = "Skill Operation Cancelled"
-                            contentText = message
-                            break;
-                        case 'AMAZON.FallbackIntent':
-                            message = "I'm not sure I understand. Please say a valid command or repeat yourself."
-                            titleText = "Skill Fallback"
-                            contentText = "Unknown command. Please say a valid command or repeat if valid."
-                            break;
-                        default:
-                            statusCode = 500;
-                            shouldEnd = true;
-                            break;
-                    }
-                }
-            });
-        }); 
+        return true
     }
 
-    // send response
+    switch (req.body.request.intent.name) {
+        case 'GetRecordIntent':
+            message = "Test get record"
+            titleText = "TEST"
+            contentText = message
+            break;
+        case 'AMAZON.HelpIntent':
+            message = "To get general records, say 'Get records'. To get records from a specific area, say 'Get records in AREA', substituting in the specific area. " +
+                "To get records with critical status, say 'Get critical records'. To exit, say 'Stop'. To cancel operation without exiting, say 'Cancel'."
+            titleText = "Skill Help Information"
+            contentText = message
+            break;
+        case 'AMAZON.StopIntent':
+            shouldEnd = true;
+            message = "Skill stopped. Shutting down."
+            titleText = "Skill Operation Stopped"
+            contentText = "Skill operation stopped. Please relaunch to continue."
+            break;
+        case 'AMAZON.CancelIntent':
+            message = "Skill operation cancelled. Please say another command to continue or 'stop' to exit."
+            titleText = "Skill Operation Cancelled"
+            contentText = message
+            break;
+        case 'AMAZON.FallbackIntent':
+            message = "I'm not sure I understand. Please say a valid command or repeat yourself."
+            titleText = "Skill Fallback"
+            contentText = "Unknown command. Please say a valid command or repeat if valid."
+            break;
+        default:
+            statusCode = 500;
+            shouldEnd = true;
+            res.status(500)
+            return false;
+
+    }
+
+    const pool = new sql.ConnectionPool(config)
+    try {
+        await pool.connect()
+        const result = await pool.request().query('select TOP 2 * from ##TempTable')
+        console.log(result)
+        sendResult(200, 'Hello', 'Some result', 'Some content');
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+        return false
+    }
+    return true
+
+})
+
+const server = app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
+
+
+function sendResult(statusCode, message, titleText, contentText) {
     res.status(statusCode).json({
         version: "1.0",
         response: {
@@ -130,10 +139,4 @@ app.post('/', async (req, res) => {
             shouldEndSession: shouldEnd
         }
     });
-})
-
-const server = app.listen(port, () => {
-    console.log('Server is running on port ' + port);
-});
-
-
+}
